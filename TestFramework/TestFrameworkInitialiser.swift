a3d6734e348b7cf8a2e744f19aaa799f183a9690
Annmarie-ds <annmarie.desilva@placie.com>
2023-07-04 17:04:13 +1000

    Initial project setup

---
+++ TestFramework/TestFrameworkInitialiser.swift
@@ -0,0 +1,14 @@
+//
+//  TestFrameworkInitialiser.swift
+//  TestFramework
+//
+//  Created by Annmarie De Silva on 4/7/2023.
+//
+
+import Foundation
+
+public struct TestFrameworkInitialiser {
+    public init(textToPrint: String) {
+        print("This is the text to print \(textToPrint)")
+    }
+}
